04530263f17d58ed228d4490ee500a11e2df860d
Tiago Costa <tiagocostacj@gmail.com>
2025-03-25 06:57:57 -0300

    modules
    ok

--- src/componentes/Topo/index.js
+++ src/componentes/Topo/index.js
@@ -1,15 +1,29 @@
-import style from "./Topo.module.css";
+'use client';
+import { useState } from 'react';
+import Link from 'next/link';
+import styles from './Topo.module.css';
 
 export default function Topo() {
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuAberto(!menuAberto);
+  };
+
   return (
-    <header className={style.topo}>
-      <h1 className={style.logo}>Pace Certo</h1>
+    <header className={styles.topo}>
+      <div className={styles.logo}>Pace Certo</div>
+
+      <button className={styles.menuToggle} onClick={toggleMenu}>
+        {menuAberto ? 'Fechar' : 'Menu'}
+      </button>
+
       <nav>
-        <ul className={style.menu}>
-          <li><a href="#corridas">Corridas</a></li>
-          <li><a href="#dicas">Dicas</a></li>
-          <li><a href="#parcerias">Parcerias</a></li>
-          <li><a href="#contato">Contato</a></li>
+        <ul className={`${styles.menu} ${menuAberto ? styles.ativo : ''}`}>
+          <li><Link href="#corridas">Corridas</Link></li>
+          <li><Link href="#dicas">Dicas</Link></li>
+          <li><Link href="#parcerias">Parcerias</Link></li>
+          <li><Link href="#contato">Contato</Link></li>
         </ul>
       </nav>
     </header>
