adee00dbf6973422bba3e3f30ba39fa22b7057eb
Tiago Costa <tiagocostacj@gmail.com>
2025-04-18 14:37:59 -0300

    datas
    datas

--- src/componentes/Card/index.js
+++ src/componentes/Card/index.js
@@ -10,72 +10,6 @@ export default function Card() {
       
       <div className={styles.container}>
         <div className={styles.card}>
-          <p><strong>Meia maratona de Curitiba</strong></p>
-          <img src="/meiaCuritiba.png" alt="Meia Maratona de Curitiba" />
-          <p>15/06/2025</p>
-          <strong><p>Curitiba</p></strong>
-          <strong><p>05 km | 11 km | 21 Km</p></strong>
-          <Link href="/corridas/meiaMaratonaDeCuritiba" legacyBehavior>
-            <a><button>Informações</button></a>
-          </Link>
-        </div>
-
-        <div className={styles.card}>
-          <p><strong>Corrida da Polícia Científica</strong></p>
-          <img src="/policiacientifica.webp" alt="Corrida da polícia científica do Paraná" />
-          <p>01/06/2025</p>
-          <strong><p>Curitiba</p></strong>
-          <strong><p>03 km | 05 km | 10 Km</p></strong>
-          <Link href="/corridas/corridaPoliciaCientifica" legacyBehavior>
-            <a><button>Informações</button></a>
-          </Link>
-        </div>
-
-        <div className={styles.card}>
-          <p><strong>3ª Etapa Corridas Curitiba</strong></p>
-          <img src="/corridascuritiba.jpg" alt="Corridas Curitiba" />
-          <p>22/06/2025</p>
-          <strong><p>São José dos Pinhais</p></strong>
-          <strong><p>05 km | 10 Km</p></strong>
-          <Link href="/corridas/corridasCuritiba" legacyBehavior>
-            <a><button>Informações</button></a>
-          </Link>
-        </div>
-
-        <div className={styles.card}>
-          <p><strong>XIII CORRIDA DA RONE</strong></p>
-          <img src="/corridaRoneBaner.png" alt="Corrida da Rone" />
-          <p>27/07/2025</p>
-          <strong><p>Curitiba</p></strong>
-          <strong><p>07 Km</p></strong>
-          <Link href="/corridas/corridaDaRone" legacyBehavior>
-            <a><button>Informações</button></a>
-          </Link>
-        </div>
-
-        <div className={styles.card}>
-          <p><strong>2025</strong></p>
-          <img src="/corridaIncrivel.png" alt="Corrida do Forte" />
-          <p>22/06/2025</p>
-          <p><strong>Curitiba</strong></p>
-          <strong><p>05 km | 10 Km |</p></strong>
-          <Link href="/corridas/corridaIncrivel" legacyBehavior>
-            <a><button>Informações</button></a>
-          </Link>
-        </div>
-
-        <div className={styles.card}>
-          <p><strong>CORRIDAS SANEPAR</strong></p>
-          <img src="/sanepar.png" alt="Corrida do Forte" />
-          <p><strong>07/12/2025</strong></p>
-          <p><strong>Curitiba</strong></p>
-          <strong><p> KIDS | 05 km | 10 Km |</p></strong>
-          <Link href="/corridas/corridasSanepar" legacyBehavior>
-            <a><button>Informações</button></a>
-          </Link>
-        </div>
-
-        <div className={styles.card}>
           <p><strong>Circuito de Corridas Procorrer</strong></p>
           <img src="/procorrer.jpg" alt="Procorrer" />
           <p>04/05/2025</p>
@@ -93,6 +27,74 @@ export default function Card() {
           <p><strong>Curitiba</strong></p>
           <strong><p>07 km</p></strong>
           <Link href="/corridas/corridaCavalaria" legacyBehavior>
+            <a><button>Informações</button></a>
+          </Link>
+        </div>
+
+        <div className={styles.card}>
+          <p><strong>Corrida da Polícia Científica</strong></p>
+          <img src="/policiacientifica.webp" alt="Corrida da polícia científica do Paraná" />
+          <p>01/06/2025</p>
+          <strong><p>Curitiba</p></strong>
+          <strong><p>03 km | 05 km | 10 Km</p></strong>
+          <Link href="/corridas/corridaPoliciaCientifica" legacyBehavior>
+            <a><button>Informações</button></a>
+          </Link>
+        </div>
+
+        <div className={styles.card}>
+          <p><strong>Meia maratona de Curitiba</strong></p>
+          <img src="/meiaCuritiba.png" alt="Meia Maratona de Curitiba" />
+          <p>15/06/2025</p>
+          <strong><p>Curitiba</p></strong>
+          <strong><p>05 km | 11 km | 21 Km</p></strong>
+          <Link href="/corridas/meiaMaratonaDeCuritiba" legacyBehavior>
+            <a><button>Informações</button></a>
+          </Link>
+        </div>
+
+        <div className={styles.card}>
+          <p><strong>3ª Etapa Corridas Curitiba</strong></p>
+          <img src="/corridascuritiba.jpg" alt="Corridas Curitiba" />
+          <p>22/06/2025</p>
+          <strong><p>São José dos Pinhais</p></strong>
+          <strong><p>05 km | 10 Km</p></strong>
+          <Link href="/corridas/corridasCuritiba" legacyBehavior>
+            <a><button>Informações</button></a>
+          </Link>
+        </div>
+
+        <div className={styles.card}>
+          <p><strong>2025</strong></p>
+          <img src="/corridaIncrivel.png" alt="Corrida do Forte" />
+          <p>22/06/2025</p>
+          <p><strong>Curitiba</strong></p>
+          <strong><p>05 km | 10 Km |</p></strong>
+          <Link href="/corridas/corridaIncrivel" legacyBehavior>
+            <a><button>Informações</button></a>
+          </Link>
+        </div>
+
+        <div className={styles.card}>
+          <p><strong>XIII CORRIDA DA RONE</strong></p>
+          <img src="/corridaRoneBaner.png" alt="Corrida da Rone" />
+          <p>27/07/2025</p>
+          <strong><p>Curitiba</p></strong>
+          <strong><p>07 Km</p></strong>
+          <Link href="/corridas/corridaDaRone" legacyBehavior>
+            <a><button>Informações</button></a>
+          </Link>
+        </div>
+
+        
+
+        <div className={styles.card}>
+          <p><strong>CORRIDAS SANEPAR</strong></p>
+          <img src="/sanepar.png" alt="Corrida do Forte" />
+          <p><strong>07/12/2025</strong></p>
+          <p><strong>Curitiba</strong></p>
+          <strong><p> KIDS | 05 km | 10 Km |</p></strong>
+          <Link href="/corridas/corridasSanepar" legacyBehavior>
             <a><button>Informações</button></a>
           </Link>
         </div>
