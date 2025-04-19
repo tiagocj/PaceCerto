e4277fa17aedf6ca5e1c798cb49df714d3df318b
Tiago Costa <tiagocostacj@gmail.com>
2025-04-19 15:50:51 -0300

    SO
    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,38 +19,41 @@ export default function Home() {
   return (
     <>
      <Head>
-  {/* Meta tags essenciais */}
-  <meta charSet="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Meta tags essenciais */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-  {/* Título com foco em SEO local */}
-  <title>Pace Certo - Corridas de Rua no Paraná</title>
+        {/* Título com foco em SEO local */}
+        <title>Pace Certo - Corridas de rua no Paraná</title>
 
-  {/* Descrição rica em palavras-chave */}
-  <meta
-    name="description"
-    content="O Pace Certo é o guia de corridas de rua no Paraná. Veja datas, locais e inscrições para provas em Curitiba, Maringá, Londrina e outras cidades."
-  />
+        {/* Descrição rica em palavras-chave */}
+        <meta
+          name="description"
+          content="O Pace Certo é o guia de corridas de rua no Paraná. Veja datas, locais e inscrições para provas em Curitiba, Maringá, Londrina e outras cidades."
+        />
 
-  {/* Palavras-chave opcionais (alguns dizem que o Google ignora, mas Bing ainda usa) */}
-  <meta
-    name="keywords"
-    content="corridas de rua Paraná, calendário de corridas Curitiba, meia maratona Londrina, provas de 5km Maringá, correr no Paraná"
-  />
+        {/* Palavras-chave opcionais (alguns dizem que o Google ignora, mas Bing ainda usa) */}
+        <meta
+          name="keywords"
+          content="corridas de rua Paraná, calendário de corridas Curitiba, meia maratona Londrina, provas de 5km Maringá, correr no Paraná"
+        />
 
-  {/* Autor (opcional, mas pode reforçar identidade) */}
-  <meta name="author" content="Tiago Jerônimo" />
+        {/* Autor (opcional, mas pode reforçar identidade) */}
+        <     meta name="author" content="Tiago Jerônimo" />
 
-  {/* Google Ads para monetização */}
-  <script 
-    async 
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9081276319826306"
-    crossOrigin="anonymous">
-  </script>
-</Head>
-      {/* Conteúdo principal da página */}
-      <main>
-        
+        {/* Google Ads para monetização */}
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9081276319826306"
+          crossOrigin="anonymous">
+        </script>
+      </Head>
+        {/* Conteúdo principal da página */}
+        <main>
+        <section>
+        <h1 className="visually-hidden">Seu calendário de corridas de rua em Curitiba, Pinhais, são josé dos pinhais no Paraná com o Pace Certo Corridas de Rua</h1>
+
+        </section>
 
         <section id="corridas">
           {/* Cards com informações sobre corridas */}
